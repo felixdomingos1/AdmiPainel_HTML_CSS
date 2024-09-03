34b2a07720bddc3c4824e29ca6003b531ec79a2c
Félix Domingos <97100392+felixdomingos1@users.noreply.github.com>
2024-09-03 04:19:59 -0700

    reatualizei para o JS

---
+++ ToJacinta/assets/js/main.js
@@ -0,0 +1,22 @@
+const showSidebar = (toggleId, sidebarId, mainId) =>{
+   const toggle = document.getElementById(toggleId),
+   sidebar = document.getElementById(sidebarId),
+   main = document.getElementById(mainId)
+
+   if(toggle && sidebar && main){
+       toggle.addEventListener('click', ()=>{
+           sidebar.classList.toggle('show-sidebar')
+           main.classList.toggle('main-pd')
+       })
+   }
+}
+showSidebar('header-toggle','sidebar', 'main')
+
+const sidebarLink = document.querySelectorAll('.sidebar__link')
+
+function linkColor(){
+    sidebarLink.forEach(l => l.classList.remove('active-link'))
+    this.classList.add('active-link')
+}
+
+sidebarLink.forEach(l => l.addEventListener('click', linkColor))
